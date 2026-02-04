86bec71664af105a6e434aec10f5acf95002b798
lucas-sousadev <lucasavitar89@gmail.com>
2026-02-04 15:56:33 -0300

    Update erro de menu

--- src/script.js
+++ src/script.js
@@ -6,3 +6,10 @@ hamMenu.addEventListener("click", () => {
   hamMenu.classList.toggle("active");
   offScreenMenu.classList.toggle("active");
 });
+
+const solucoesBtn = document.querySelector(".solucoes-btn");
+const solucoesSubmenu = document.querySelector(".solucoes-submenu");
+
+solucoesBtn.addEventListener("click", () => {
+	solucoesSubmenu.classList.toggle("active");
+});
