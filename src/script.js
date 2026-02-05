d72df0489455809c09e7821fa7299c0f1ca5dd17
lucas-sousadev <lucasavitar89@gmail.com>
2026-02-05 11:48:06 -0300

    adição de animação na pagina inicial

--- src/script.js
+++ src/script.js
@@ -13,3 +13,35 @@ const solucoesSubmenu = document.querySelector(".solucoes-submenu");
 solucoesBtn.addEventListener("click", () => {
 	solucoesSubmenu.classList.toggle("active");
 });
+
+
+// animaçao das palavras na pagina inicial
+const words = ['responde', 'cresce', 'prospera', 'irradia', 'impacta'];
+		let wordIndex = 0;
+		let charIndex = 0;
+		let isDeleting = false;
+		const wordElement = document.querySelector('.word-change');
+
+		function typeWord() {
+		    const currentWord = words[wordIndex];
+		    
+		    if (isDeleting) {
+		        charIndex--;
+		    } else {
+		        charIndex++;
+		    }
+		    
+		    wordElement.textContent = currentWord.substring(0, charIndex);
+		    
+		    if (!isDeleting && charIndex === currentWord.length) {
+		        setTimeout(() => { isDeleting = true; }, 1500);
+		    } else if (isDeleting && charIndex === 0) {
+		        isDeleting = false;
+		        wordIndex = (wordIndex + 1) % words.length;
+		    }
+		    
+		    const speed = isDeleting ? 50 : 100;
+		    setTimeout(typeWord, speed);
+		}
+
+		typeWord();
